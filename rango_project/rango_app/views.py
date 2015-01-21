dd9363fd0d5f33dbd805b9810c6329da739b70f1
2077762b <2077762b@BO706-20.ad.dcs.gla.ac.uk>
2015-01-21 12:55:09 +0000

    Updated Progress

--- rango_project/rango_app/views.py
+++ rango_project/rango_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango_app.models import Category
+from rango_app.models import Page
 
 from django.http import HttpResponse
 
@@ -12,3 +13,17 @@ def index(request):
 def about(request):
     context_dict = {'name': "Chris Brown", 'number': "2077762b" }
     return render(request, 'rango/about.txt', context_dict)
+
+def category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.txt', context_dict)
