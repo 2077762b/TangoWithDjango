b491fea5b23e0725660b44d9609864c0074e0519
2077762b <2077762b@BO706-24.ad.dcs.gla.ac.uk>
2015-01-29 10:59:18 +0000

    Updated Progress

--- rango_project/rango_app/views.py
+++ rango_project/rango_app/views.py
@@ -3,6 +3,7 @@ from rango_app.models import Category
 from rango_app.models import Page
 from rango_app.forms import CategoryForm
 from rango_app.forms import PageForm
+from rango_app.forms import UserForm, UserProfileForm
 
 from django.http import HttpResponse
 
@@ -68,3 +69,37 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form, 'category': cat, 'category_name_slug':category_name_slug }
 
     return render(request, 'rango/add_page.txt', context_dict)
+	
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+       
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'rango/register.txt',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
